1244cec8bd4280c0072ce510c0c54fbb538b3d5c
Artem <artemstark1554@gmail.com>
2024-08-19 23:05:31 +0300

    first-try

--- precode.go
+++ precode.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -42,11 +44,83 @@ var tasks = map[string]Task{
 // Ниже напишите обработчики для каждого эндпоинта
 // ...
 
+func getAllTasks(w http.ResponseWriter, r *http.Request) {
+	//Сериализируем данные запроса
+	resp, err := json.Marshal(tasks)
+	//Обрабатываем ошибку
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	//Установливается заголовок JSON
+	w.Header().Set("Content-type", "application/json")
+	//Установка статуса 200, успешный запрос
+	w.WriteHeader(http.StatusOK)
+	//Отправка данных клиенту
+	w.Write(resp)
+}
+
+func postTasks(w http.ResponseWriter, r *http.Request) {
+	//Создаются переменные: task - для хранения десериализованных данных. buf - это буфер для временного хранения данных из тела запроса.
+	var task Task
+	var buf bytes.Buffer
+
+	_, err := buf.ReadFrom(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if err := json.Unmarshal(buf.Bytes(), &task); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	tasks[task.ID] = task
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+}
+
+func getTaskId(w http.ResponseWriter, r *http.Request) {
+	//Получаем id нужно задачи из URL
+	id := chi.URLParam(r, "id")
+	task, ok := tasks[id]
+	if !ok {
+		http.Error(w, "Задача не найдена", http.StatusNoContent)
+		return
+	}
+	resp, err := json.Marshal(task)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(resp)
+}
+
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	//первая переменная пропускается, так как мы ничего не запрашиваем у сервера и ничего не добавляем. Следовательно и сериализация не нужна.
+	_, ok := tasks[id]
+	if !ok {
+		http.Error(w, "Задача не найдена", http.StatusNotFound)
+		return
+	}
+	//Используется для удаления
+	delete(tasks, id)
+	//нужно задать статус 204
+	w.WriteHeader(http.StatusNoContent)
+}
+
 func main() {
 	r := chi.NewRouter()
 
 	// здесь регистрируйте ваши обработчики
-	// ...
+	r.Get("/tasks", getAllTasks)
+
+	r.Post("/tasks", postTasks)
+
+	r.Get("/tasks/{id}", getTaskId)
+
+	r.Delete("/tasks/{id}", deleteTask)
 
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		fmt.Printf("Ошибка при запуске сервера: %s", err.Error())
